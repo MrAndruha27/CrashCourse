6398cea82f3346cd219b1efd1ea459f4d127644f
MrAndruha27 <koolmaster03@gmail.com>
2023-03-07 21:45:59 +0200

    dz

--- task17/Program.cs
+++ task17/Program.cs
@@ -8,25 +8,7 @@ namespace task17
         {
 
 
-            ///НЕДОРОБЛЕНО
-            string Stone = "";
-            int i = 0;
-            while (i != 1)
-            {
-              
-                Console.WriteLine("Введiть слово ");
-                Stone += Console.ReadLine();
-         //       Console.WriteLine(Stone.Substring(Stone.LastIndexOf('.') ), Stone.LastIndexOf('.')+1));
-                if (Stone.Substring((0),1) == ".")
-                {
-                    i = 1;
-                }
-            }
-            string[] words = Stone.Split(' ');
-            for (int ii = 0; ii < words.Length; ii++)
-            {
-                Console.Write(words[i]+", ");
-            }
+           
 
         }
         }
---
+++ task17/Weapon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task17
+{
+    class Weapon
+    {
+
+       readonly double ShootRange;
+       readonly double Calibr;
+
+        int CurrectAmmo { get; set; }
+
+        int MaxAmmo { get; set; }
+      
+        public Weapon(int range, float caliber, int maxSize)
+        {
+            ShootRange = range;
+            Calibr = caliber;
+            MaxAmmo = maxSize;
+        }
+        bool Shot()
+        {
+            if (CurrectAmmo <= 0)
+            {
+                CurrectAmmo--;
+                return false;
+                    }
+
+            else
+            {
+                Console.WriteLine("Not enough  ammo!");
+                return true;
+            }
+          
+        }
+        void Recharge()
+        {
+            CurrectAmmo = MaxAmmo;
+        }
+    }
+}
