9f0192a39e7d06333d1c970545f3119b3508cbe6
MrAndruha27 <koolmaster03@gmail.com>
2023-03-18 13:03:21 +0200

    dz

---
+++ task18/Auto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task18
+{
+    class Auto
+    {
+        private const int Wheels=4;
+        private readonly int MaxSpeed;
+        private double Price;
+        private readonly double MaxFluel;
+        private  double CurrectFluel;
+        private bool Avalible;
+        private double? AvgPrice;
+
+       public Auto(int maxSpeed,double price, double maxFluel, bool avalible)
+        {
+            if (maxSpeed < 0)
+                MaxSpeed = 0;
+            else
+            MaxSpeed = maxSpeed;
+
+            if (price < 0)
+                Price = 0;
+            else
+                Price = price;
+
+            if (maxFluel < 0)
+                MaxFluel = 0;
+            else
+                MaxFluel = maxFluel;
+            Avalible = avalible;
+        }
+      public  Auto(int maxSpeed, double price, double maxFluel,double currfluel, bool avalible)
+        {
+            if (maxSpeed < 0)
+                MaxSpeed = 0;
+            else
+                MaxSpeed = maxSpeed;
+
+            if (price < 0)
+                Price = 0;
+            else
+                Price = price;
+
+            if (maxFluel < 0)
+                MaxFluel = 0;
+            else
+                MaxFluel = maxFluel;
+
+            if (currfluel < 0)
+                CurrectFluel = 0;
+            else
+                CurrectFluel = currfluel;
+            Avalible = avalible;
+        }
+        public void Info()
+            {
+            Console.WriteLine($"Max speed: {MaxSpeed}\nPrice: {Price}\nCurrect fluel: {CurrectFluel}\nMax fluel: {MaxFluel}\nAvalible: {Avalible}\n");
+        }
+        public void BuyFuel()
+            {
+            CurrectFluel=MaxFluel;
+        }
+        public void SetDiscount()
+        {
+            Price = Price - (Price / 30);
+        }
+    }
+}
---
+++ task18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car1 = new Auto(120,35000,30,true);
+            car1.Info();
+            car1.BuyFuel();
+            car1.Info();
+            car1.SetDiscount();
+            car1.Info();
+        }
+    }
+}
